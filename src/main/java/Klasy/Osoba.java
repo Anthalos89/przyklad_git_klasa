f1276845edf92cbe7c861cbe3f68f410ba573fb9
Kuba <kuba@wp.pl>
2023-04-22 11:51:33 +0200

    22.04.23

--- src/main/java/Klasy/Osoba.java
+++ src/main/java/Klasy/Osoba.java
@@ -1,12 +1,14 @@
 package Klasy;
 
 public class Osoba {
-    public String imie;
-    public String nazwisko;
+    private String imie;
+    private String nazwisko;
 
     public Osoba(String imie, String nazwisko) {
         this.imie = imie;
         this.nazwisko = nazwisko;
     }
-
+    public String getInfo(){
+        return "Imie: " + this.imie + "\n" + "Nazwisko: " + this.nazwisko;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,23 @@ import Klasy.Osoba;
 
 public class Main {
     public static void main(String[] args) {
-
+/*
         Osoba osoba = new Osoba("Adam", "Kowalski");
-        System.out.println("Imię: " + osoba.imie + "\n" + "Nazwisko:" +osoba.nazwisko);
+        System.out.println("Imię: " + osoba.imie + "\n" + "Nazwisko:" + osoba.nazwisko);
         Osoba osoba2 = new Osoba("Amelia", "Nowak");
-        System.out.println("Imię: " + osoba2.imie + "\n" + "Nazwisko:" +osoba2.nazwisko);
+        System.out.println("Imię: " + osoba2.imie + "\n" + "Nazwisko:" + osoba2.nazwisko);
+    */ /*
+    dla
+    public class Osoba {
+        public String imie;
+        public String nazwisko;
+    */
+        Osoba osoba = new Osoba("Adam", "Kowalski");
+        System.out.println(osoba.getInfo());
+        Osoba osoba2 = new Osoba("Amelia", "Nowak");
+        System.out.println(osoba2.getInfo());
+        Osoba osoba3 = new Osoba("Alicja", "Bark");
+        System.out.println(osoba3.getInfo());
+        System.out.println("Lolipop");
     }
 }
